8068645b538a739b159eabc4ddaa6c55e50722b5
iammaxrodriguez <mrodx360@comcast.net>
2022-11-26 14:24:04 -0500

    starting project

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">The Learning Attic</h1>
+      <p>Welcome to The Learning Attic</p>
+    </>
+  );
+};
+
+export default Index;
